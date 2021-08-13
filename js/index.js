e48be13ace5a892cd44d554b1961c5db546753a5
Prakhar Verma <68277409+rxi-prakharvrm@users.noreply.github.com>
2021-08-13 10:59:44 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,20 @@
+// let fixedSidePanel = document.querySelector(".fixed-side-panel");
+// let header = document.querySelector(".header");
+
+// window.onscroll = function () {
+//   scrollFunction();
+// };
+
+// function scrollFunction() {
+//   if (document.documentElement.scrollTop >= 3000) {
+//     fixedSidePanel.style.display = "none";
+//   } else if (document.documentElement.scrollTop < 3000) {
+//     fixedSidePanel.style.display = "block";
+//   }
+
+//   if (document.documentElement.scrollTop >= 80) {
+//     header.style.padding = "2rem 0";
+//   } else if (document.documentElement.scrollTop < 80) {
+//     header.style.padding = "5rem 2rem";
+//   }
+// }
